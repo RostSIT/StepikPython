af437fb42f5e0bca33c948416275cb6d60006fdc
RostSIT <streltsovrp@gmail.com>
2021-09-06 22:42:58 +0300

    import requests (only works in console) add comments

--- thirdWeek/3.6 Установка дополнительных модулей 1 из 3 шагов пройден.py
+++ thirdWeek/3.6 Установка дополнительных модулей 1 из 3 шагов пройден.py
@@ -2,3 +2,5 @@ import requests
 r = requests.get('http://example.com')  # Простой гет запрос
 print(r.text)  # Вывод ответа от сервера
 #  import requests (only works in console)
+'''import requests
+ModuleNotFoundError: No module named 'requests'''
